b88eae9f1a06b677900874a4b9f3a0c21f2db6a7
Vidrea Sebastian <vidreasebastian@gmail.com>
2021-11-01 21:26:11 +0200

    add Picker over the increments array

--- 03/06/Action/Action/ContentView.swift
+++ 03/06/Action/Action/ContentView.swift
@@ -1,7 +1,10 @@
 import SwiftUI
 
 struct ContentView {
+  let increments = [1, 2, 3, 5, 10]
   @State private var currentValue = 0
+  @State private var incrementIndex = 0
+  @State private var increment = 10
   @State private var horizontalSpace = 0.0
 }
 
@@ -15,20 +18,24 @@ extension ContentView: View {
         SymbolButton("arrow.right",
                      action: forward)
       }
-      Text(horizontalSpace.description)
-      Slider(value: $horizontalSpace,
-             in: 0...100)
-        .padding(.horizontal)
+      Picker("Choose the increment",
+             selection: $incrementIndex) {
+        ForEach(increments.indices) { index in
+          Text("by \(increments[index])")
+        }
+      }
+      .pickerStyle(.segmented)
+      .padding()
     }
   }
 }
 
 extension ContentView {
   private func back() {
-    currentValue -= 1
+    currentValue -= increments[incrementIndex]
   }
   private func forward() {
-    currentValue += 1
+    currentValue += increments[incrementIndex]
   }
 }
 
